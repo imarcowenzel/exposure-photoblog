c04b1b6e7085f8a11d4a6430fa779e6c7242685f
imarcowenzel <marcowenzelcirilo@gmail.com>
2024-05-02 11:36:30 -0300

    Updating

--- app/(root)/profile/[username]/components/posts-feed.tsx
+++ app/(root)/profile/[username]/components/posts-feed.tsx
@@ -11,7 +11,7 @@ export const PostsFeed = ({ posts }: Props) => {
   return (
     <>
       <h2 className="bg-primary px-4 py-1 text-primary">Gallery</h2>
-      <div className="grid w-fit h-full grid-cols-1 items-center justify-center gap-6 md:grid-cols-4 md:gap-8 2xl:grid-cols-5">
+      <div className="grid w-fit grid-cols-1 items-center justify-center gap-6 md:grid-cols-4 md:gap-8 2xl:grid-cols-5">
         {posts.map((post) => (
           <figure key={post.id} className="flex flex-col gap-y-2">
             <Link href={`/post/${post.id}`}>
