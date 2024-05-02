b8bd71f0501089320033004afa15c0fb93f53b78
imarcowenzel <marcowenzelcirilo@gmail.com>
2024-05-02 15:34:23 -0300

    Updating

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -51,14 +51,14 @@ const SearchPage = async ({
   }
 
   return (
-    <section className="flex w-full flex-col items-center gap-10 px-5 py-16 md:px-16 2xl:h-[calc(100dvh-470px)] 2xl:px-24">
+    <section className="flex w-full flex-col items-center gap-10 px-5 py-16 md:px-16 2xl:h-dvh 2xl:px-24">
+
       <div className="flex flex-col">
         <SearchForm />
         <Separator className="h-[2px]" />
       </div>
 
       {!results ? null : !!results.length ? (
-        // TODO: put the profile user at the base of the post
         <PostsFeed posts={results} />
       ) : (
         <p>No results found!</p>
