5b68e8eba94ad4e09f1b58e692ed53c6f58a3321
imarcowenzel <marcowenzelcirilo@gmail.com>
2024-05-02 16:04:40 -0300

    Updating

--- app/(root)/search/components/no-results.tsx
+++ app/(root)/search/components/no-results.tsx
@@ -2,7 +2,7 @@ import { HeartCrackIcon } from "lucide-react";
 
 export const NoResults = () => {
   return (
-    <div className="flex h-full flex-col gap-5 w-full items-center justify-center">
+    <div className="flex h-full flex-col gap-5 w-full items-center justify-center py-10 2xl:py-0">
       <HeartCrackIcon height={48} width={48} color="#FF0000" />
       <h3 className="text-center xl:text-xl">Sorry, no results were found.</h3>
     </div>
--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -53,7 +53,7 @@ const SearchPage = async ({
     <section
       className={cn(
         "flex w-full flex-col items-center gap-10 px-5 py-16 md:px-16 2xl:h-dvh 2xl:px-24",
-        results && "h-full",
+        results.lenght === 0 && "h-full",
       )}
     >
       <div className="flex flex-col">
@@ -61,7 +61,7 @@ const SearchPage = async ({
         <Separator className="h-[2px]" />
       </div>
 
-      {!results ? null : !!results.length ? (
+      {!results ? null : results.length !== 0 ? (
         <PostsFeed posts={results} />
       ) : (
         <NoResults />
